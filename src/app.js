9ed696a8649e2ec0c08be873d05ba88f61637346
Ujjwal Akash Chintu <ujjwalakash01@gmil.com>
2020-02-04 21:57:36 +0530

    Setup an app for heroku

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
-const port=process.env.port||3000
+const port = process.env.PORT ||3000
 // it will make an enviornmental variable . It will available on heroku. If it fail we will provide the default value of port 3000
 
 // Define paths for Express config
@@ -99,5 +99,5 @@ app.get('*', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log('Server is up on port'+port)
+    console.log('Server is up on port' +port)
 })
