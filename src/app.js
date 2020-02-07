96048e7c7a2c11e2d6069948a4e6d36c888161b2
Ujjwal Akash Chintu <ujjwalakash01@gmil.com>
2020-02-07 23:01:52 +0530

    changes in email

--- src/app.js
+++ src/app.js
@@ -4,6 +4,12 @@ const hbs = require('hbs')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
+//mail contains
+const bodyParser=require('body-parser');
+//const exphbs= require ('express-handlebars');
+const nodemailer=require ('nodemailer');
+
+
 const app = express()
 const port = process.env.PORT || 3000
 // it will make an enviornmental variable . It will available on heroku. If it fail we will provide the default value of port 3000
@@ -12,6 +18,8 @@ const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
+
+app.use(bodyParser.urlencoded({extended:false})); // for email
 
 // Setup handlebars engine and views location
 app.set('view engine', 'hbs')
@@ -37,6 +45,7 @@ app.get('/about', (req, res) => {
 
 app.get('/help', (req, res) => {
     res.render('help', {
+        layout: false,
         helpText: 'This is some helpful text.',
         title: 'Help',
         name: 'Ujjwal Akash'
@@ -96,6 +105,52 @@ app.get('*', (req, res) => {
         name: 'Andrew Mead',
         errorMessage: 'Page not found.'
     })
+})
+
+
+app.post('/send',(req,res) => {
+    
+    const output =
+    `<p> New help message with following details : </p>
+    <ul>
+        <li>Email : ${req.body.email}</li>
+        <li>Message : ${req.body.message}</li>
+            <li>Mobile no : ${req.body.mobile}</li>
+                </ul>
+                `
+                var transporter = nodemailer.createTransport({
+                    service: 'gmail',
+                    
+                  
+                    auth: {
+                      user: 'ujjwalakash147@gmail.com',
+                      pass: 'Ujjwal@123'
+                    },
+                    tls:{
+                        rejectUnauthorized:false
+                    }
+                  });
+                  //transporter will tell which client we will use to send a mail
+                  var mailOptions = {
+                    from: req.body.email,
+                    to: 'ujjwalakash147@gmail.com',
+                    subject: 'Sending Email using Node.js',
+                    text: 'That was easy!',
+                    html: output// output
+                  };
+                  // mail option is an object
+                  transporter.sendMail(mailOptions, function(error, info){
+                    if (error) {
+                      console.log(error);
+                    } else {
+                      console.log('Email sent: ' + info.response);
+                   
+                    }
+                   
+                  
+                   //res.render('contact',{layout: false,msg:`Email has senr`});
+                  });
+    
 })
 
 app.listen(port, () => {
